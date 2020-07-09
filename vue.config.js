1b1f883de04ff9e05a5c82103f8d3dfd6a505465
chenglijuan1611 <chenglijuan_1611@163.com>
2020-07-09 14:00:27 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
   assetsDir: "static",
   // 是否开启eslint保存检测，有效值：ture | false | 'error'
-  lintOnSave: process.env.NODE_ENV === "development",
+  lintOnSave: process.env.NODE_ENV === "false",
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: true,
   // webpack-dev-server 相关配置
@@ -33,7 +33,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://47.114.84.236:8066`,
+        target: `http://172.16.60.184:8066`,
         changeOrigin: true,
         pathRewrite: {
           ["^" + process.env.VUE_APP_BASE_API]: ""
