96dcd6c374383c6c8d7fb71cc58a4c700fc50cb4
chenglijuan1611 <chenglijuan_1611@163.com>
2020-07-15 11:10:08 +0800

    终端用户信息 修改列表 搜索条件

--- src/views/system/customer/index.vue
+++ src/views/system/customer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" :inline="true" label-width="68px">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" label-width="auto">
       <el-form-item label="客户账号" prop="customer">
         <el-input
           v-model="queryParams.customer"
@@ -34,7 +34,7 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-     
+
       <el-form-item label="省" prop="province">
         <el-input
           v-model="queryParams.province"
@@ -63,21 +63,30 @@
         />
       </el-form-item>
       <el-form-item label="创建时间" prop="gmtCreatetime">
-        <el-date-picker clearable size="small" style="width: 200px"
+        <!-- <el-date-picker clearable size="small" style="width: 200px"
           v-model="queryParams.gmtCreatetime"
           type="date"
           value-format="yyyy-MM-dd"
           placeholder="选择创建时间">
-        </el-date-picker>
+        </el-date-picker>-->
+        <el-date-picker
+          value-format="yyyy-MM-dd"
+          v-model="time"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          size="small"
+        />
       </el-form-item>
-      <el-form-item label="修改时间" prop="gmtModifytime">
+      <!-- <el-form-item label="修改时间" prop="gmtModifytime">
         <el-date-picker clearable size="small" style="width: 200px"
           v-model="queryParams.gmtModifytime"
           type="date"
           value-format="yyyy-MM-dd"
           placeholder="选择修改时间">
         </el-date-picker>
-      </el-form-item>
+      </el-form-item>-->
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
@@ -85,7 +94,7 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
+      <!-- <el-col :span="1.5">
         <el-button
           type="primary"
           icon="el-icon-plus"
@@ -113,7 +122,7 @@
           @click="handleDelete"
           v-hasPermi="['system:customer:remove']"
         >删除</el-button>
-      </el-col>
+      </el-col>-->
       <el-col :span="1.5">
         <el-button
           type="warning"
@@ -125,19 +134,19 @@
       </el-col>
     </el-row>
 
-    <el-table v-loading="loading" :data="customerList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
+    <el-table v-loading="loading" :data="customerList">
+      <!-- <el-table-column type="selection" width="55" align="center" /> -->
       <el-table-column label="客户账号" align="center" prop="customer" />
       <el-table-column label="性别" align="center" prop="sex" />
       <el-table-column label="设备名称" align="center" prop="name" />
       <el-table-column label="手机号" align="center" prop="phone" />
-      <el-table-column label="是否是维修" align="center" prop="isRepair" />
-      <!-- <el-table-column label="省" align="center" prop="province" />
+      <!-- <el-table-column label="是否是维修" align="center" prop="isRepair" /> -->
+      <el-table-column label="省" align="center" prop="province" />
       <el-table-column label="市" align="center" prop="city" />
       <el-table-column label="区" align="center" prop="district" />
-      <el-table-column label="详细地址" align="center" prop="address" />
-      <el-table-column label="头像" align="center" prop="avatar" /> -->
-      <el-table-column label="是否是测试" align="center" prop="isTest" />
+      <!-- <el-table-column label="详细地址" align="center" prop="address" /> -->
+      <!-- <el-table-column label="头像" align="center" prop="avatar" /> -->
+      <!-- <el-table-column label="是否是测试" align="center" prop="isTest" /> -->
       <el-table-column label="创建时间" align="center" prop="gmtCreatetime" width="180">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.gmtCreatetime) }}</span>
@@ -148,28 +157,26 @@
           <span>{{ parseTime(scope.row.gmtModifytime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="openid" align="center" prop="openid" />
-      <el-table-column label="是否登录" align="center" prop="isLogin" />
+      <!-- <el-table-column label="openid" align="center" prop="openid" /> -->
+      <!-- <el-table-column label="是否登录" align="center" prop="isLogin" /> -->
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
             size="mini"
             type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['system:customer:edit']"
-          >修改</el-button>
+            icon="el-icon-location"
+            @click="detailaddress(scope.row)"
+          >详细地址</el-button>
           <el-button
             size="mini"
             type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['system:customer:remove']"
-          >删除</el-button>
+            icon="el-icon-s-fold"
+            @click="bdevicelist(scope.row)"
+          >已绑定设备</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
+
     <pagination
       v-show="total>0"
       :total="total"
@@ -179,7 +186,7 @@
     />
 
     <!-- 添加或修改客户信息对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" :close-on-click-modal="false">
+    <!-- <el-dialog :title="title" :visible.sync="open" width="500px" :close-on-click-modal="false">
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="性别">
           <el-select v-model="form.sex" placeholder="请选择性别">
@@ -218,20 +225,26 @@
           <el-input v-model="form.isTest" placeholder="请输入是否是测试" />
         </el-form-item>
         <el-form-item label="创建时间" prop="gmtCreatetime">
-          <el-date-picker clearable size="small" style="width: 200px"
+          <el-date-picker
+            clearable
+            size="small"
+            style="width: 200px"
             v-model="form.gmtCreatetime"
             type="date"
             value-format="yyyy-MM-dd"
-            placeholder="选择创建时间">
-          </el-date-picker>
+            placeholder="选择创建时间"
+          ></el-date-picker>
         </el-form-item>
         <el-form-item label="修改时间" prop="gmtModifytime">
-          <el-date-picker clearable size="small" style="width: 200px"
+          <el-date-picker
+            clearable
+            size="small"
+            style="width: 200px"
             v-model="form.gmtModifytime"
             type="date"
             value-format="yyyy-MM-dd"
-            placeholder="选择修改时间">
-          </el-date-picker>
+            placeholder="选择修改时间"
+          ></el-date-picker>
         </el-form-item>
         <el-form-item label="openid" prop="openid">
           <el-input v-model="form.openid" placeholder="请输入openid" />
@@ -244,15 +257,22 @@
         <el-button type="primary" @click="submitForm">确 定</el-button>
         <el-button @click="cancel">取 消</el-button>
       </div>
-    </el-dialog>
+    </el-dialog>-->
   </div>
 </template>
 
 <script>
-import { listCustomer, getCustomer, delCustomer, addCustomer, updateCustomer, exportCustomer } from "@/api/system/customer";
+import {
+  listCustomer,
+  getCustomer,
+  delCustomer,
+  addCustomer,
+  updateCustomer,
+  exportCustomer
+} from '@/api/system/customer'
 
 export default {
-  name: "Customer",
+  name: 'Customer',
   data() {
     return {
       // 遮罩层
@@ -268,7 +288,7 @@ export default {
       // 客户信息表格数据
       customerList: [],
       // 弹出层标题
-      title: "",
+      title: '',
       // 是否显示弹出层
       open: false,
       // 查询参数
@@ -289,33 +309,41 @@ export default {
         gmtCreatetime: undefined,
         gmtModifytime: undefined,
         openid: undefined,
-        isLogin: undefined
+        isLogin: undefined,
+        beginTime: undefined,
+        endTime: undefined
       },
       // 表单参数
       form: {},
       // 表单校验
-      rules: {
-      }
-    };
+      rules: {},
+      time: undefined
+    }
   },
   created() {
-    this.getList();
+    this.getList()
   },
   methods: {
+    detailaddress(x) {
+      console.log(x)
+    },
+    bdevicelist(x) {
+      console.log(x)
+    },
     /** 查询客户信息列表 */
     getList() {
-      this.loading = true;
+      this.loading = true
       listCustomer(this.queryParams).then(response => {
-        this.customerList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
+        this.customerList = response.rows
+        this.total = response.total
+        this.loading = false
+      })
     },
     // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
+    // cancel() {
+    //   this.open = false
+    //   this.reset()
+    // },
     // 表单重置
     reset() {
       this.form = {
@@ -334,97 +362,118 @@ export default {
         gmtCreatetime: undefined,
         gmtModifytime: undefined,
         openid: undefined,
-        isLogin: undefined
-      };
-      this.resetForm("form");
+        isLogin: undefined,
+        beginTime: undefined,
+        endTime: undefined
+      }
+      this.time = undefined
+      this.resetForm('form')
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
-      this.getList();
+      if (this.time) {
+        this.queryParams.beginTime = this.time[0]
+        this.queryParams.endTime = this.time[1]
+      }
+      this.queryParams.pageNum = 1
+      this.getList()
     },
     /** 重置按钮操作 */
     resetQuery() {
-      this.resetForm("queryForm");
-      this.handleQuery();
+      this.queryParams.beginTime = undefined
+      this.queryParams.endTime = undefined
+      this.time = undefined
+      this.resetForm('queryForm')
+      this.handleQuery()
     },
     // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.customer)
-      this.single = selection.length!=1
-      this.multiple = !selection.length
-    },
+    // handleSelectionChange(selection) {
+    //   this.ids = selection.map(item => item.customer)
+    //   this.single = selection.length != 1
+    //   this.multiple = !selection.length
+    // },
     /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加客户信息";
-    },
+    // handleAdd() {
+    //   this.reset()
+    //   this.open = true
+    //   this.title = '添加客户信息'
+    // },
     /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const customer = row.customer || this.ids
-      getCustomer(customer).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改客户信息";
-      });
-    },
+    // handleUpdate(row) {
+    //   this.isadd = false
+    //   this.reset()
+    //   const customer = row.customer || this.ids
+    //   getCustomer(customer).then(response => {
+    //     this.form = response.data
+    //     this.open = true
+    //     this.title = '修改客户信息'
+    //   })
+    // },
     /** 提交按钮 */
-    submitForm: function() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.customer != undefined) {
-            updateCustomer(this.form).then(response => {
-              if (response.code === 200) {
-                this.msgSuccess("修改成功");
-                this.open = false;
-                this.getList();
-              } else {
-                this.msgError(response.msg);
-              }
-            });
-          } else {
-            addCustomer(this.form).then(response => {
-              if (response.code === 200) {
-                this.msgSuccess("新增成功");
-                this.open = false;
-                this.getList();
-              } else {
-                this.msgError(response.msg);
-              }
-            });
-          }
-        }
-      });
-    },
+    // submitForm: function() {
+    //   this.$refs['form'].validate(valid => {
+    //     if (valid) {
+    //       if (this.form.customer != undefined) {
+    //         updateCustomer(this.form).then(response => {
+    //           if (response.code === 200) {
+    //             this.msgSuccess('修改成功')
+    //             this.open = false
+    //             this.getList()
+    //           } else {
+    //             this.msgError(response.msg)
+    //           }
+    //         })
+    //       } else {
+    //         addCustomer(this.form).then(response => {
+    //           if (response.code === 200) {
+    //             this.msgSuccess('新增成功')
+    //             this.open = false
+    //             this.getList()
+    //           } else {
+    //             this.msgError(response.msg)
+    //           }
+    //         })
+    //       }
+    //     }
+    //   })
+    // },
     /** 删除按钮操作 */
-    handleDelete(row) {
-      const customers = row.customer || this.ids;
-      this.$confirm('是否确认删除客户信息编号为"' + customers + '"的数据项?', "警告", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning"
-        }).then(function() {
-          return delCustomer(customers);
-        }).then(() => {
-          this.getList();
-          this.msgSuccess("删除成功");
-        }).catch(function() {});
-    },
+    // handleDelete(row) {
+    //   const customers = row.customer || this.ids
+    //   this.$confirm(
+    //     '是否确认删除客户信息编号为"' + customers + '"的数据项?',
+    //     '警告',
+    //     {
+    //       confirmButtonText: '确定',
+    //       cancelButtonText: '取消',
+    //       type: 'warning'
+    //     }
+    //   )
+    //     .then(function() {
+    //       return delCustomer(customers)
+    //     })
+    //     .then(() => {
+    //       this.getList()
+    //       this.msgSuccess('删除成功')
+    //     })
+    //     .catch(function() {})
+    // },
     /** 导出按钮操作 */
     handleExport() {
-      const queryParams = this.queryParams;
-      this.$confirm('是否确认导出所有客户信息数据项?', "警告", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning"
-        }).then(function() {
-          return exportCustomer(queryParams);
-        }).then(response => {
-          this.download(response.msg);
-        }).catch(function() {});
+      const queryParams = this.queryParams
+      this.$confirm('是否确认导出所有客户信息数据项?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(function() {
+          return exportCustomer(queryParams)
+        })
+        .then(response => {
+          this.download(response.msg)
+        })
+        .catch(function() {})
     }
   }
-};
+}
 </script>
